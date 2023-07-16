2e01e550d20171d642302c49b490fdea3ffe3f0a
Olya <olha.pad@gmail.com>
2023-07-16 16:39:46 +0200

    lesson #4 and #5

--- bot/src/settings.py
+++ bot/src/settings.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.WARNING)
 load_dotenv()
 
+
 try:
     BOT_TOKEN = os.environ["BOT_TOKEN"]
 except KeyError as err:
-    logging.critical(f"Can`t read token from environment variable. Message: {err}")
+    logging.critical(f"Can`t read tocken from enviroment variable. Message: {err}")
     raise KeyError(err)
